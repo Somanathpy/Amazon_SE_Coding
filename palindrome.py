dc0fadddfe472a7c621d1d55369fa5fbd2ec7534
Somanath Patil <patilsomnath86@gmail.com>
2017-02-01 01:20:25 -0600

    Palindrome code with and wthout function usage

--- palindrome.py
+++ palindrome.py
@@ -1,21 +1,30 @@
-'''check to see if a given string is a palindrome or not'''
-def is_palindrome(str):
-	l = len(str)
-	list_s = list(str)
-	for i in  range(0,l):
-		if list_s[i] != list_s[l-i-1]:
-			return False
-	return True
-str = input("enter a string:")
-if is_palindrome(str):
-	print(str +" is a Palindrome")
-else:
-	print(str+" is not a palindrome")
-	
-	
+#without a function
 '''
-str = input("enter a string:")
-if str == str[::-1]:
+str = input("enter a word:")
+str = list(str)
+l = len(str)
+tester = None
+for i in range(0,l):
+	if str[i] != str[l-i-1]:
+		tester = False
+	else:
+		tester = True
+if tester:
 	print("Palindrome")
 else:
-	print("not a palindrome")'''
+	print("Not a palindrome")'''
+	
+#with a function
+def is_palindrome(word):
+	#word = input("enter a word")
+	word = list(word)
+	l = len(word)
+	for i in range(0,l):
+		if word[i] != word[l-i-1]:
+			return False
+	return True
+word = input("enter a string: ")	
+if is_palindrome(word):
+	print(word+"  is a Palindrome ")
+else:
+	print(word+"  is not a palindrome")
